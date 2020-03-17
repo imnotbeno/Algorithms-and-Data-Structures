185635cc593beef04ff1b87c5d1646f37caffe43
Niko Bec <nikobec96@gmail.com>
2020-03-17 20:21:04 +0100

    Bubble sort algorithm

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,1 +1,14 @@
 #Bubble sort algorithm exercise
+
+l = [5, 1, 4, 2, 8]
+
+def bubble_sort(arr):
+    for i in range(len(l)):
+        for j in range(0, len(l)-i-1):
+            if l[j] > l[j+1]:
+                l[j], l[j+1] = l[j+1], l[j]
+
+bubble_sort(l)
+
+for i in range(len(l)):
+    print(l[i])
