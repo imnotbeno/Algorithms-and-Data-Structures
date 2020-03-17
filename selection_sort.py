4155b780150912706750c7d571704c5335083234
Niko Bec <nikobec96@gmail.com>
2020-03-17 18:20:46 +0100

    Added a selection sorting algorithm

---
+++ selection_sort.py
@@ -0,0 +1,13 @@
+#selection sorting
+arr = [64,25,12,22,11]
+
+for i in range(len(arr)):
+    min_idx = i
+    for j in range(i+1, len(arr)):
+        if arr[min_idx] > arr[j]:
+            min_idx = j
+    arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+print("Sorted array")
+for i in range(len(arr)):
+    print(arr[i])
