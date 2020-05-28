91f98b21467e7bd2003ade564fc505604a71d1d8
Niko Bec <nikobec96@gmail.com>
2020-05-28 18:43:00 +0200

    Added a fibonacci search algorithm and a binary tree implementation

---
+++ binary_tree.py
@@ -0,0 +1,11 @@
+# !python3
+# Binary tree
+
+class Node:
+
+    def __init__(self, data):
+        self.key = data
+        self.right = right
+        self.left = left
+
+# Driver code
---
+++ fibonacci_search.py
@@ -0,0 +1,9 @@
+#! python3
+
+list = [2, 3, 4, 10, 40]
+x = 4
+fibonacci_numbers  = [1, 1, 2, 3, 5, 8, 13]
+length = len(list)
+
+for i in fibonacci_numbers:
+    if length <= i:
