86cbc79ecc637ccd9113b4d4f759c4f12fff128c
Niko Bec <nikobec96@gmail.com>
2020-05-28 19:10:04 +0200

    Changes to binary tree

--- binary_tree.py
+++ binary_tree.py
@@ -5,7 +5,20 @@ class Node:
 
     def __init__(self, data):
         self.key = data
-        self.right = right
-        self.left = left
+        self.right = None #They're set to None because we need to add nodes to the tree
+        self.left = None
+
+def printInorder(node):
+
+    if node:
+        printInorder(node.left)
+        print(node.key)
+        printInorder(node.right)
 
 # Driver code
+if __name__ == "__main__":
+    root = Node(1)
+    root.right = Node(3)
+    root.left = Node(2)
+
+    printInorder(root)
