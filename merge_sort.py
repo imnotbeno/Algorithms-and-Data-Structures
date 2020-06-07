b68785608428b17111027c7f054b4dd2a8d3cdda
Niko Bec <nikobec96@gmail.com>
2020-06-07 12:41:49 +0200

    Fixed merge_sort bug

--- merge_sort.py
+++ merge_sort.py
@@ -1,52 +1,45 @@
 # !python3
 # divide and conquer algorithm
 
-def mergesort(arr):
+def mergeSort(arr):
     if len(arr) > 1:
         mid = len(arr)//2
         left = arr[:mid]
         right = arr[mid:]
 
-        # Merge sort for the left half
-        mergesort(left)
-        # Merge sort for the right half
-        mergesort(right)
+        mergeSort(left)
+        mergeSort(right)
 
-        i= j= k= 0
+        i=j=k=0
 
         while i < len(left) and j < len(right):
             if left[i] < right[j]:
                 arr[k] = left[i]
                 i+=1
             else:
-                arr[k] < right[j]
+                arr[k] = right[j]
                 j+=1
             k+=1
-
-        # Checking if any elements remain
+        
         while i < len(left):
             arr[k] = left[i]
             i+=1
             k+=1
-
+        
         while j < len(right):
             arr[k] = right[j]
             j+=1
             k+=1
 
 def printList(arr):
-    for i in range(len(arr)):
-        print(arr[i],end=' ')
+    for each in range(len(arr)):
+        print(arr[each], end=" ")
     print()
 
 if __name__ == "__main__":
     a = [12, 11, 13, 5, 6, 7]
-    l = [3, 1, 8, 5, 10, 9, 7, 6, 4, 2]
+    print("Given array is: ")
     printList(a)
-    mergesort(a)
+    mergeSort(a)
+    print("Sorted array is: ")
     printList(a)
-
-#left = l[:m]
-#right = l[m:]
-#print(left)
-#print(right)
