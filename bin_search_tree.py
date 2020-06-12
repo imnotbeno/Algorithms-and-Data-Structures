2c395416338b8ea0ed44ec072dd1ef4889338b15
Niko Bec <nikobec96@gmail.com>
2020-06-12 16:45:02 +0200

    Added search function

--- bin_search_tree.py
+++ bin_search_tree.py
@@ -8,11 +8,31 @@ class Node:
         self.right = None
 
 
+# Search function
+def search(root, val):
+
+    if root is None or root.key == val:
+        return root
+
+    if root.key < val:
+        return search(root.right, val)
+        
+    if root.key > val:
+        return search(root.left, val) 
+
+
 # Driver code
 
 if __name__ == "__main__":
 
-    root = Node(10)
-    
+    root = Node(6)
+    root.right = Node(10)
+    root.left = Node(3)
+    root.left.right = Node(4)
+    root.left.left = Node(1)
+    root.right.right = Node(12)
+    root.right.left = Node(8)
 
+    search(root, 10)
+    
 
