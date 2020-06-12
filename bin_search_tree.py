3440af4a48d578e32afb0d1596f7fcde803ffa80
Niko Bec <nikobec96@gmail.com>
2020-06-12 16:28:35 +0200

    initial commit

---
+++ bin_search_tree.py
@@ -0,0 +1,18 @@
+# !python3
+
+class Node:
+
+    def __init__(self, key):
+        self.key = key 
+        self.left = None
+        self.right = None
+
+
+# Driver code
+
+if __name__ == "__main__":
+
+    root = Node(10)
+    
+
+
