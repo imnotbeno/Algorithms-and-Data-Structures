3a5654f9ba73ffa2c32cedf840ea28b04659c465
Niko Bec <nikobec96@gmail.com>
2020-06-12 17:18:09 +0200

    Tried fixing bugs, got bored. Will finish tomorrow

--- bin_search_tree.py
+++ bin_search_tree.py
@@ -48,14 +48,19 @@ def inorder(root):
 # Driver code
 if __name__ == "__main__":
 
-    root = Node(6)
-    root.right = Node(10)
-    root.left = Node(3)
-    root.left.right = Node(4)
-    root.left.left = Node(1)
-    root.right.right = Node(12)
-    root.right.left = Node(8)
+    # root = Node(6)
+    # root.right = Node(10)
+    # root.left = Node(3)
+    # root.left.right = Node(4)
+    # root.left.left = Node(1)
+    # root.right.right = Node(12)
+    # root.right.left = Node(8)
+    r = Node(50) 
+    insert(r,Node(30)) 
+    insert(r,Node(20)) 
+    insert(r,Node(40)) 
+    insert(r,Node(70)) 
+    insert(r,Node(60)) 
+    insert(r,Node(80)
 
-    search(root, 10)
-    
-
+    #inorder(r)
