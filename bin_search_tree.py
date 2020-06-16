6b41af1c6c7c9c2e20c4813bc4abd6e2da164b9b
Niko Bec <nikobec96@gmail.com>
2020-06-16 18:45:00 +0200

    Fixed EOF error

--- bin_search_tree.py
+++ bin_search_tree.py
@@ -1,66 +1,62 @@
 # !python3
 
-class Node:
-
-    def __init__(self, key):
-        self.key = key 
+class Node: 
+    def __init__(self,key): 
         self.left = None
         self.right = None
+        self.val = key 
 
+def search(root,key): 
+      
+    # Base Cases: root is null or key is present at root 
+    if root is None or root.val == key: 
+        return root 
+  
+    # Key is greater than root's key 
+    if root.val < key: 
+        return search(root.right,key) 
+    
+    # Key is smaller than root's key 
+    return search(root.left,key)   
 
-# Search function
-def search(root, val):
-
-    if root is None or root.key == val:
-        return root
-
-    if root.key < val:
-        return search(root.right, val)
-        
-    if root.key > val:
-        return search(root.left, val) 
-
-# Insertion function
-def insert(root, node):
-    if root is None:
-        root = node
-    else:
-        if root.key < node.key:
-            if root.right == None:
+# Node insert function 
+def insert(root,node): 
+    if root is None: 
+        root = node 
+    else: 
+        if root.val < node.val: 
+            if root.right is None: 
                 root.right = node 
             else: 
-                insert(root, node)
-
-        if root.key > node.key:
-            if root.left == None:
+                insert(root.right, node) 
+        else: 
+            if root.left is None: 
                 root.left = node 
             else: 
-                insert(root, node)
-
-# Inorder traversal/print function
-def inorder(root):
-    if root:
-        inorder(root.left)
-        print(root.key)
-        inorder(root.right)
-    
-
-# Driver code
-if __name__ == "__main__":
-
-    # root = Node(6)
-    # root.right = Node(10)
-    # root.left = Node(3)
-    # root.left.right = Node(4)
-    # root.left.left = Node(1)
-    # root.right.right = Node(12)
-    # root.right.left = Node(8)
-    r = Node(50) 
-    insert(r,Node(30)) 
-    insert(r,Node(20)) 
-    insert(r,Node(40)) 
-    insert(r,Node(70)) 
-    insert(r,Node(60)) 
-    insert(r,Node(80)
-
-    #inorder(r)
+                insert(root.left, node) 
+  
+# Inorder tree traversal 
+def inorder(root): 
+    if root: 
+        inorder(root.left) 
+        print(root.val) 
+        inorder(root.right) 
+  
+  
+# Driver program to test the above functions 
+# Let us create the following BST 
+#      50 
+#    /      \ 
+#   30     70 
+#   / \    / \ 
+#  20 40  60 80 
+r = Node(50) 
+insert(r,Node(30)) 
+insert(r,Node(20)) 
+insert(r,Node(40)) 
+insert(r,Node(70)) 
+insert(r,Node(60)) 
+insert(r,Node(80)) 
+  
+# Print inoder traversal of the BST 
+inorder(r) 
