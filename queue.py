5499d86ae2627787975f8fd690190d736b0e8bdd
Niko Bec <nikobec96@gmail.com>
2020-06-19 12:17:15 +0200

    added enqueue function

--- queue.py
+++ queue.py
@@ -11,5 +11,19 @@ class Queue:
     def qFull(self):
         return self.size == self.capacity
 
-    def qEmpty():
+    def qEmpty(self):
         return self.size == 0
+
+    # Function to add an item into the queue
+    def enqueue(self, item):
+        if self.qFull():
+            print("Warning: the queue is full")
+            return
+        self.rear = (self.rear + 1) % (self.capacity)
+        self.Q[self.rear] = item 
+        self.size = self.size + 1
+        print("% s has been enqueued" % str(item))
+
+if __name__ == "__main__":
+    queue = Queue(30)
+    queue.enqueue(10)
