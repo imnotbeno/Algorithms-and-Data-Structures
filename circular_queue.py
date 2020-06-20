aad025eeab92884206863c7b163787ed8c0d9d78
Niko Bec <nikobec96@gmail.com>
2020-06-20 14:29:00 +0200

    added enqueue function

--- circular_queue.py
+++ circular_queue.py
@@ -8,3 +8,23 @@ class CircularQueue():
         self.size = size
         self.queue = [None for i in range(size)]
         self.front = self.rear = -1
+
+    # Function to insert item into queue
+    def enqueue(self, item):
+        
+        # Check if queue is full 
+        if(self.rear + 1) % self.size == self.front:
+            print("Queue is full!")
+
+        # Empty queue
+        elif self.front == -1:
+            self.front = 0
+            self.rear = 0
+            self.queue[self.rear] = item
+
+        else:
+            self.rear = (self.rear + 1) % self.size
+            self.queue[self.rear] = item
+
+    # Function to delete item in queue
+    #def dequeue(self):    
