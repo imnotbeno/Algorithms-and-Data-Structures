5fb97e86810803b15fd27de01a145ec7dd9ed997
Niko Bec <nikobec96@gmail.com>
2020-06-20 17:21:15 +0200

    added a display function

--- circular_queue.py
+++ circular_queue.py
@@ -29,7 +29,7 @@ class CircularQueue():
     # Function to delete item in queue
     def dequeue(self):    
         if self.front == -1:
-            print("Queue is empty")
+            print("Queue is empty!")
 
         # Condition for only one element
         elif self.front == self.rear:
@@ -42,6 +42,30 @@ class CircularQueue():
             tmp = self.queue[self.front]
             self.front = (self.front + 1) % self.size
             return tmp
+
+    # Function to display the queue
+    def display(self):
+        
+        if self.front == -1:
+            print("The queue is empty!")
+
+        elif self.rear >= self.front:
+            print("Elements in circular queue are: ", end=" ")
+
+            for i in range(self.front, self.rear + 1):
+                print(self.queue[i], end=" ")
+            print()
+
+        else:
+            print("Elements in circular queue are: ", end=" ")
+
+            for i in range(self.front, self.size):
+                print(self.queue[i], end=" ")
+            for i in range(0, self.rear + 1):
+                print(self.queue[i], end=" ")
+        
+        if (self.rear + 1) % self.size == self.front:
+            print("Queue is full!")
 
 
 # Driver code
