04c6b7403c8e2922928c140845254df52485aad9
Niko Bec <nikobec96@gmail.com>
2020-06-23 16:14:18 +0200

    added a function to print graph + driver code

--- graph.py
+++ graph.py
@@ -22,3 +22,19 @@ class Graph:
         node.next = self.graph[destination]
         self.graph[destination] = node
 
+    def print_graph(self):
+        for i in range(self.V):
+            print("Adjacency list of vertex {}\n ".format(i), end=" ")
+            tmp = self.graph[i]
+            print(" -> {}".format(tmp.vertex), end=" ")
+            tmp = tmp.next
+        
+if __name__ == "__main__":
+
+    graph = Graph(5)
+    graph.add_edge(0,1)
+    graph.add_edge(1,2)
+    graph.add_edge(2,3)
+    graph.add_edge(3,4)
+
+    graph.print_graph()
