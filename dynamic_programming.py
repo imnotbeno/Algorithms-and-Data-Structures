43ae3a3eb297994f6c48a5c199b19520acbf76ac
Niko Bec <nikobec96@gmail.com>
2020-07-03 11:28:57 +0200

    Tabulation method for fibonacci sequence

--- dynamic_programming.py
+++ dynamic_programming.py
@@ -2,6 +2,7 @@
 
 # Memoization of nth fibonacci number, using a lookup table
 
+'''
 def fib(n, lookup):
 
     # bas case
@@ -19,6 +20,25 @@ def main():
 
     lookup = [None] * 101
     print ("Fibonacci number is: ", fib(n, lookup))
+
+'''
+
+
+# Tabulation method of same example
+
+def fib(n):
+
+    f = [0]*(n+1)
+
+    f[1] = 1
+
+    for i in range(2, n+1):
+        f[i] = f[i-1] + f[i-2]
+    return f[n]
+
+def main():
+    n = 34
+    print("Fibonacci number is: ", fib(n))
 
 main()
 
