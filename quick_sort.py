a76aa1154c35fb72fdfbab1a70f334e001f3a39a
Niko Bec <nikobec96@gmail.com>
2021-01-19 13:18:30 +0100

    added quicksort algo

---
+++ quick_sort.py
@@ -0,0 +1,25 @@
+l = [10, 80, 30, 90, 50, 60, 40, 20, 70]
+n = len(l)
+
+def partitioning(arr,low,high):
+    i = ( low-1 )         # index of smaller element
+    pivot = arr[high]     # pivot
+
+    for j in range(low , high):
+        if   arr[j] < pivot:
+            i = i+1
+            arr[i],arr[j] = arr[j],arr[i]
+
+    arr[i+1],arr[high] = arr[high],arr[i+1]
+    return ( i+1 ) #return pivot index
+
+def quickSort(arr,low,high):
+    if low < high:
+        pi = partitioning(arr,low,high)
+        quickSort(arr, low, pi-1)
+        quickSort(arr, pi+1, high)
+
+quickSort(l,0,n-1)
+print("Sorted array: ")
+for x in range(n):
+    print(l[x])
